1a03f89ad7c80a50794ceefc68ad72330e164099
Hidayath-S <hiddu91@gmail.com>
2019-01-07 21:04:28 +0530

    Adding Verifyer(PACT Test)

---
+++ verifyer/src/main/java/se/ff/bsv/verifyer/App.java
@@ -0,0 +1,13 @@
+package se.ff.bsv.verifyer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
