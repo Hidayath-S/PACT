9d1dec52c868917b7ab676bfd06bf8582fcf382a
Hidayath-S <hiddu91@gmail.com>
2019-01-09 21:09:07 +0530

    updates

---
+++ clientUpdated/src/main/java/se/ff/bsc/users_POSTClient.java
@@ -0,0 +1,58 @@
+package se.ff.bsc;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+public class users_POSTClient {
+	int port=8081;
+	
+	users_POSTClient(){
+		System.out.println("Default port "+port);
+	}
+	
+	users_POSTClient(int port){
+		this.port=port;
+        System.out.println("Custom port "+port);
+		
+	}
+	
+	public static void main(String[] args) {
+		String response=new users_POSTClient().getUserDetails();
+	}
+	
+	
+	
+	
+	
+	
+	public String getUserDetails() {
+		String url=String.format("https://reqres.in//api/users");
+		System.out.println("using the URL="+url);
+		try {
+			HttpResponse r=Request.Get(url).addHeader("Accept","application/json").execute().returnResponse();
+		String json=EntityUtils.toString(r.getEntity());
+		System.out.println("Json="+json);
+		JSONObject jsonobj=new JSONObject(json);
+		String fName=jsonobj.get("firstName").toString();
+		return fName;
+		
+		
+		
+		
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return url;
+		
+	}
+	
+
+}
---
+++ clientUpdated/src/test/java/se/ff/bsc/getCountryClientTest.java
@@ -0,0 +1,59 @@
+package se.ff.bsc;
+import au.com.dius.pact.consumer.Pact;
+import au.com.dius.pact.consumer.PactProviderRuleMk2;
+import au.com.dius.pact.consumer.PactVerification;
+import au.com.dius.pact.consumer.dsl.DslPart;
+import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.model.RequestResponsePact;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class getCountryClientTest {
+	@Rule
+	public PactProviderRuleMk2 provider= new PactProviderRuleMk2("getCountryService","localhost",1234,this); 
+	
+@Pact(consumer="getCountryServiceClient")
+public RequestResponsePact createPact(PactDslWithProvider builder) {
+	Map<String, String> headers=new HashMap();
+	headers.put("Content-Type", "application/json");
+	
+	
+	DslPart userDetails= new PactDslJsonBody()
+			.stringType("name","India")
+			.stringType("alpha2_code","IN")
+			.stringType("alpha3_code","IND")
+			.asBody();
+	
+	return builder
+			.given("There is a country with alpha2_code as IN having name as India")
+            .uponReceiving("A request for alpha2_code IN")
+            .path("/country/get/iso3code/IND")
+            .method("GET")
+            .willRespondWith().body(userDetails)
+            .status(200)
+            .headers(headers)
+            .toPact();
+	
+}
+
+@Test()
+@PactVerification()
+public void doTest() {
+	System.setProperty("pact.rootDir","../pacts");
+	String cName= new getCountryClient(provider.getPort()).getCountryDetails();
+	System.out.println("According to test fName="+cName);
+	
+	
+}
+
+
+}
